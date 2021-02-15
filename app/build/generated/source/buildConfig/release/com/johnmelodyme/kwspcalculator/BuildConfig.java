d3f4a3311979cb97edd615892528a39395a95c70
John Melody Me <johnmelodyme@yandex.com>
2021-02-16 01:06:37 +0800

    no message

--- app/build/generated/source/buildConfig/release/com/johnmelodyme/kwspcalculator/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/johnmelodyme/kwspcalculator/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.johnmelodyme.kwspcalculator";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 2;
-  public static final String VERSION_NAME = "2.0";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "3.0";
 }
